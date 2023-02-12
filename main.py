d081748074ab070bbee9c1076269e86490c2b3dc
221RDB301 <118080877+221RDB301@users.noreply.github.com>
2023-02-12 14:24:29 +0200

    git commit

--- main.py
+++ main.py
@@ -13,17 +13,33 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
+            opening_brackets_stack.append(Bracket(next, i))
             # Process opening bracket, write your code here
-            pass
+            #pass
 
         if next in ")]}":
+            if not opening_brackets_stack:
+                return i + 1
+            top = opening_brackets_stack.pop()
+            if not are_matching(top.char, next):
+                return i + 1
             # Process closing bracket, write your code here
-            pass
+            #pass
+
+    if opening_brackets_stack:
+        top = opening_brackets_stack.pop()
+        return top.position + 1
+    
+    return "Success"
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
+    if isinstance(mismatch, int):
+        print(mismatch)
+    else:
+        print("Success")
     # Printing answer, write your code here
 
 
